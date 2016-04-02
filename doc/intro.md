c7784f97724afba486410b1c569f7b5c79c91446
Shriphani Palakodety <shriphanip@gmail.com>
2016-04-01 23:08:19 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to thrown-test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/thrown_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns thrown-test.core-test
+  (:require [clojure.test :refer :all]
+            [thrown-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
