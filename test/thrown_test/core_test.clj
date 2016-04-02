693fd9ab31e5139fb2b303aba7c8e61951f75469
Shriphani Palakodety <shriphanip@gmail.com>
2016-04-01 23:11:35 -0700

    test to repro

--- test/thrown_test/core_test.clj
+++ test/thrown_test/core_test.clj
@@ -2,6 +2,31 @@
   (:require [clojure.test :refer :all]
             [thrown-test.core :refer :all]))
 
+(defn throw-not-caught
+  []
+  (map
+   (fn [x]
+     (when (= x 50)
+       (throw "AAAAAH! 50!")))
+   (range 100)))
+
+(defn throw-caught
+  []
+  (doall
+   (map
+    (fn [x]
+      (when (= x 50)
+        (throw "AAAAAH! 50!")))
+    (range 100))))
+
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Is the exception caught"
+    (is
+     (thrown?
+      Exception
+      (throw-not-caught)))
+
+    (is
+     (thrown?
+      Exception
+      (throw-caught)))))
